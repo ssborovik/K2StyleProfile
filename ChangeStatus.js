cc30b14b1c49bbd0e44703d69cf0d5260ed46799
ssborovik <92776983+ssborovik@users.noreply.github.com>
2025-08-01 14:07:46 +0300

    Update ChangeStatus.js
    New Status

--- ChangeStatus.js
+++ ChangeStatus.js
@@ -1,27 +1,155 @@
-<script>
-  const css = `
-    .theme-entry .grid-content-cell-wrapper[title="Завершен"] .runtime-list-item a {
-      background-color: #23b962 !important;
-    }
-    .theme-entry .grid-content-cell-wrapper[title="Отклонен"] .runtime-list-item a {
-      background-color: #e74c3c !important;
-    }
-    .theme-entry .runtime-list-item a {
-      border-radius: 8px !important;
-      text-decoration: none !important;
-      color: white !important;
-      padding: 2px 6px !important;
-      display: inline-block !important;
-      line-height: 1.2 !important;
-      font-weight: bold !important;
-      transition: transform 0.2s ease-in-out !important; /* плавность */
-    }
-    .theme-entry .runtime-list-item a:hover {
-  transform: scale(1.1); /* немного увеличивается */
-}
-  `;
+<script>const css=`
 
-  const style = document.createElement("style");
-  style.textContent = css;
-  document.head.appendChild(style);
+/* Группа завершено */
+.theme-entry .grid-content-cell-wrapper[title="Завершен"] .runtime-list-item a {
+    background-color: #23b962 !important;
+}
+
+.theme-entry .grid-content-cell-wrapper[title="Завершено"] .runtime-list-item a {
+    background-color: #23b962 !important;
+}
+
+.theme-entry .grid-content-cell-wrapper[title="st Completed"] .runtime-list-item a {
+    background-color: #23b962 !important;
+}
+
+/* Группа отклонено */
+.theme-entry .grid-content-cell-wrapper[title="Отклонен"] .runtime-list-item a {
+    background-color: #e74c3c !important;
+}
+
+.theme-entry .grid-content-cell-wrapper[title="Відхилений"] .runtime-list-item a {
+    background-color: #e74c3c !important;
+}
+
+.theme-entry .grid-content-cell-wrapper[title="st Declined"] .runtime-list-item a {
+    background-color: #e74c3c !important;
+}
+
+/* Группа На утверждении руководителем */
+.theme-entry .grid-content-cell-wrapper[title="st Approving by Chief"] .runtime-list-item a {
+    background-color: #F4A460 !important;
+}
+
+.theme-entry .grid-content-cell-wrapper[title="На утверждении руководителем"] .runtime-list-item a {
+    background-color: #F4A460 !important;
+}
+
+.theme-entry .grid-content-cell-wrapper[title="На затвердженні керівником"] .runtime-list-item a {
+    background-color: #F4A460 !important;
+}
+
+/* Группа Удален */
+.theme-entry .grid-content-cell-wrapper[title="st Deleted"] .runtime-list-item a {
+    background-color: #e74c3c !important;
+}
+
+.theme-entry .grid-content-cell-wrapper[title="Удален"] .runtime-list-item a {
+    background-color: #e74c3c !important;
+}
+
+.theme-entry .grid-content-cell-wrapper[title="Видалений"] .runtime-list-item a {
+    background-color: #e74c3c !important;
+}
+
+/* Группа Удален */
+.theme-entry .grid-content-cell-wrapper[title="st Expired"] .runtime-list-item a {
+    background-color: #F4A460 !important;
+}
+
+.theme-entry .grid-content-cell-wrapper[title="Просрочено"] .runtime-list-item a {
+    background-color: #F4A460 !important;
+}
+
+.theme-entry .grid-content-cell-wrapper[title="Прострочено"] .runtime-list-item a {
+    background-color: #F4A460 !important;
+}
+/* Группа Стартован */
+.theme-entry .grid-content-cell-wrapper[title="st Started"] .runtime-list-item a {
+    background-color: #6495ED !important;
+}
+
+.theme-entry .grid-content-cell-wrapper[title="Стартован"] .runtime-list-item a {
+    background-color: #6495ED !important;
+}
+.theme-entry .grid-content-cell-wrapper[title="Стартований"] .runtime-list-item a {
+    background-color: #6495ED !important;
+}
+
+
+/* Группа Новый */
+.grid-content-cell-wrapper[title="st New"] .runtime-list-item {
+    background-color: #6495ED !important;
+    border-radius: 8px !important;
+    color: white !important;
+    padding: 2px 6px !important;
+    display: inline-block !important;
+    /* обязательно inline-block */
+    font-weight: bold !important;
+    line-height: 1.2 !important;
+    width: auto !important;
+    /* убрать возможную ширину */
+    max-width: fit-content !important;
+    /* поджать до содержимого */
+    white-space: nowrap !important;
+    /* не переносить строки */
+}
+
+
+
+.grid-content-cell-wrapper[title="Новый"] .runtime-list-item {
+    background-color: #6495ED !important;
+    border-radius: 8px !important;
+    color: white !important;
+    padding: 2px 6px !important;
+    display: inline-block !important;
+    /* обязательно inline-block */
+    font-weight: bold !important;
+    line-height: 1.2 !important;
+    width: auto !important;
+    /* убрать возможную ширину */
+    max-width: fit-content !important;
+    /* поджать до содержимого */
+    white-space: nowrap !important;
+    /* не переносить строки */
+}
+
+.grid-content-cell-wrapper[title="Новий"] .runtime-list-item {
+    background-color: #6495ED !important;
+    border-radius: 8px !important;
+    color: white !important;
+    padding: 2px 6px !important;
+    display: inline-block !important;
+    /* обязательно inline-block */
+    font-weight: bold !important;
+    line-height: 1.2 !important;
+    width: auto !important;
+    /* убрать возможную ширину */
+    max-width: fit-content !important;
+    /* поджать до содержимого */
+    white-space: nowrap !important;
+    /* не переносить строки */
+}
+
+.theme-entry .runtime-list-item a {
+    border-radius: 8px !important;
+    text-decoration: none !important;
+    color: white !important;
+    padding: 2px 6px !important;
+    display: inline-block !important;
+    line-height: 1.2 !important;
+    font-weight: bold !important;
+    transition: transform 0.2s ease-in-out !important;
+    /* плавность */
+}
+
+.theme-entry .runtime-list-item:hover {
+    transform: scale(1.2);
+    /* немного увеличивается */
+}
+
+`;
+const style=document.createElement("style");
+style.textContent=css;
+document.head.appendChild(style);
 </script>
